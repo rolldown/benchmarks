90f50544079a31009e669ba8cefe160dcaaac027
IWANABETHATGUY <iwanabethatguy@qq.com>
2025-12-26 18:14:22 +0800

    update rolldown config

--- apps/rome/rolldown.config.mjs
+++ apps/rome/rolldown.config.mjs
@@ -5,14 +5,15 @@ export default defineConfig({
   input: {
     main: "./src/entry.ts",
   },
-	platform: "node",
+  platform: "node",
   cwd: import.meta.dirname,
-  define: {
-    "process.env.NODE_ENV": JSON.stringify("production"),
+  transform: {
+    define: {
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    },
   },
   shimMissingExports: true,
-	tsconfig: path.resolve(import.meta.dirname, "src/tsconfig.json"),
-  profilerNames: false,
+  tsconfig: path.resolve(import.meta.dirname, "src/tsconfig.json"),
   output: {
     minify: true,
     sourcemap: true,
